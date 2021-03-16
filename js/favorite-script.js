9d1cf25a9edd4e5ec0ae9664208c4d91fc21fbe1
Georgios Fragkias <geo.fragkias@gmail.com>
2021-03-16 15:28:41 +0100

    ssss

--- js/favorite-script.js
+++ js/favorite-script.js
@@ -1,9 +1,13 @@
 const favoriteIcon = document.querySelector(".heart-icon");
+const heartFull = document.querySelector(".heart-full");
 
 favoriteIcon.addEventListener("click", colourChange);
 
 function colourChange() {
-    favoriteIcon.style.color = "blue";
-
-    favoriteIcon.reset;
+    favoriteIcon.style.display = "none";
+    heartFull.style.display = "block";
 }
+
+
+// .heart-full {
+//     display: none;
