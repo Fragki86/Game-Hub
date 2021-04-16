30e54813cdbd745aaa3ebedf5a8864a7a9255f2d
Georgios Fragkias <geo.fragkias@gmail.com>
2021-04-16 23:07:38 +0200

    Update home-page.js

--- js/home-page.js
+++ js/home-page.js
@@ -1,1 +1,31 @@
-const
+const carouselContainer = document.querySelector(".carousels");
+const api = "https://georgiosf.no/game-hub-api/wp-json/wc/store/products";
+
+async function getFeatured() {
+    try {
+        const response = await fetch(api);
+        const results = await response.json();
+        console.log(results);
+        createCarousels(results);
+        
+    } catch(error) {
+        console.log("Error");
+    }
+
+}
+
+getFeatured();
+
+function createCarousels(featured) {
+    featured.forEach(function(pickGames){
+        carouselContainer.innerHTML = 
+        `<div class="carousel-mix">
+            <div id="carousel">
+                <figure>
+                    <img src="${pickGames.images[0].src}">
+                    <img src="${pickGames.images[0].src}">
+                </figure>
+            </div>
+        <div>`
+    })
+}
